3e7d40cff189bb1083eea1e321d1a8c70ab4545a
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2026-03-25 19:18:35 +0900

    fix: 빌드 오류 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <h1>ZERO100 Admin API</h1>;
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <h1>ZERO100 Admin API</h1>
+    </div>
+  );
 };
 
 export default Home;
